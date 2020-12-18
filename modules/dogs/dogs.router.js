504167c6acf24862abeeeac09a068d7a2bb6c1d4
M-DeJean <dejeantech@yahoo.com>
2020-12-18 15:27:02 -0800

    not sure what changed, app seems to be working now

--- modules/dogs/dogs.router.js
+++ modules/dogs/dogs.router.js
@@ -6,20 +6,19 @@ const People = require('../people/people.service')
 const DogsRouter = express.Router()
 
 DogsRouter
-.route('/')
-.get((req, res) => {
-  // Return all pets currently up for adoption.
-   res.status(200).json(Dogs.get())
-})
+  .route('/')
+  .get((req, res) => {
+    // Return all pets currently up for adoption.
+    res.status(200).json(Dogs.get())
+  })
 
 DogsRouter
-.route('/')
-.delete(json, (req, res) => {
-  // Remove a pet from adoption.
-  Dogs.enqueue(Dogs.dequeue())
-  People.enqueue(People.dequeue())
-  res.status(204).end()
-  
-})
+  .route('/')
+  .delete(json, (req, res) => {
+    // Remove a pet from adoption.
+    Dogs.enqueue(Dogs.dequeue())
+    People.enqueue(People.dequeue())
+    res.status(204).end()
+  })
 
 module.exports = DogsRouter
